e06ff353fb7f610cc038b915add53ff2249e7f58
ddeblander <david.deblander@condorcet.be>
2022-11-28 22:07:20 +0100

    Ajoutez des fichiers projet.

---
+++ WPF_Threads/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Threads.Model
+{
+    internal class Person
+    {
+    }
+}
---
+++ WPF_Threads/Model/Road.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_Threads.Model
+{
+    internal class Road
+    {
+        string name;
+        uint km;
+        short x, y;
+        public Road(string name,uint km, short x, short y) 
+        {
+            this.name = name;
+            if (km > 0)
+            {
+                this.km = km;
+            }
+            else
+                Console.WriteLine($"mauvaise saisie des km car négatif : {km}");
+            this.x = x;
+            this.y = y;
+
+        }
+        
+    }
+}
